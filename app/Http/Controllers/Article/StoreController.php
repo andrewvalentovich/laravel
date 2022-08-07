1598e9ab373d80779538d95a43887df3f708b6f1
Andrew <andrich_14@mail.ru>
2022-08-07 15:52:54 +0300

    Class Request

--- app/Http/Controllers/Article/StoreController.php
+++ app/Http/Controllers/Article/StoreController.php
@@ -3,23 +3,18 @@
 namespace App\Http\Controllers\Article;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Article\StoreRequest;
 use App\Models\Article;
 use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
-    public function __invoke()
+    public function __invoke(StoreRequest $request)
     {
-        $data = request()->validate([
-            'title' => 'required|string',
-            'category_id' => 'nullable',
-            'tags' => 'required',
-            'description' => 'required|string',
-            'image' => 'required|string',
-            'content' => 'required|string'
-        ]);
+        $data = $request->validated();
         $tags = $data['tags'];
         unset($data['tags']);
+
         $article = Article::create($data);
         $article->tags()->attach($tags);
 
--- app/Http/Controllers/Article/UpdateController.php
+++ app/Http/Controllers/Article/UpdateController.php
@@ -3,26 +3,21 @@
 namespace App\Http\Controllers\Article;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Article\StoreRequest;
 use App\Models\Article;
 use Illuminate\Http\Request;
 
 class UpdateController extends Controller
 {
-    public function __invoke(Article $article)
+    public function __invoke(StoreRequest $request, Article $article)
     {
-        $data = request()->validate([
-            'title' => 'required|string',
-            'category_id' => 'nullable',
-            'tags' => 'required',
-            'description' => 'required|string',
-            'image' => 'required|string',
-            'content' => 'required|string'
-        ]);
+        $data = $request->validated();
         $tags = $data['tags'];
         unset($data['tags']);
 
         $article->update($data);
         $article->tags()->sync($tags);
+
         return redirect()->route('articles.show', compact('article'));
     }
 }
